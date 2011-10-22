889c015fd77b844e10f03af79346674b6ddd2297
Cam Pedersen <diffference@gmail.com>
2011-10-21 23:59:44 -0400

    raise padding

--- zalgo.js
+++ zalgo.js
@@ -9,12 +9,12 @@
 var colors = require('colors'),
     spit = function(text, padding){
       padding = (Array(padding) || Array(2)).join('\n');
-      console.log(padding + text + padding);
+      console.log(padding + text);
     };
 
 if (!process.argv[2]) {
-  spit('usage: zalgo "he comes"'.magenta);
+  spit('usage: zalgo "he comes"'.red);
   process.exit();
 }
 
-spit(colors.zalgo(process.argv[2]).rainbow,5);
+spit(colors.zalgo(process.argv[2]).rainbow,6);
