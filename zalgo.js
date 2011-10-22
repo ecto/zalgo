11da1698538d189add55d83cbe61b5a411f5f6f3
Cam Pedersen <diffference@gmail.com>
2011-10-21 23:51:44 -0400

    DOES IT WORK

---
+++ zalgo.js
@@ -0,0 +1,19 @@
+/*
+ * zalgo
+ * wrapper for Marak's colors zalgo generator
+ * Cam Pedersen
+ * Oct 21, 2011
+ */
+
+var colors = require('colors'),
+    spit = function(text){
+      console.log('\n\n' + text + '\n\n');
+    };
+
+if (!process.argv[2]) {
+  spit('usage: zalgo "he comes"'.magenta);
+}
+
+console.log(
+  colors.zalgo(process.argv[2]).rainbow
+);
